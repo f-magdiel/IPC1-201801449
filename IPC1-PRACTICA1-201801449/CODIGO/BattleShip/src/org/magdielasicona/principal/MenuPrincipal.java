2006da0434cb4651630d8a7c91bc21028d1eeb32
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-02 17:58:59 -0600

    Se creó el mensaje de Bienvenida

--- IPC1-PRACTICA1-201801449/CODIGO/BattleShip/src/org/magdielasicona/principal/MenuPrincipal.java
+++ IPC1-PRACTICA1-201801449/CODIGO/BattleShip/src/org/magdielasicona/principal/MenuPrincipal.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.magdielasicona.principal;
 
 /**
@@ -10,5 +6,18 @@ package org.magdielasicona.principal;
  * @author FRANMAGDIEL_PC
  */
 public class MenuPrincipal {
-    
+    public static void main(String[] args) {
+        System.out.println("" +
+        "*********          ****       **************     **************     **********        ******        ***       ***     **************    *********       \n"+
+        "***    ***       ***   ***          ***               ***           ***             ***    ***      ***       ***           ***         ***     ***     \n"+
+        "***    ***      ***     ***         ***               ***           ***             ***             ***       ***           ***         ***     ***     \n"+
+        "**********     *************        ***               ***           *********       *********       *************           ***         *********       \n"+
+        "***     ***   ***        ***        ***               ***           ***                    ***      ***       ***           ***         ***             \n"+
+        "***     ***   ***        ***        ***               ***           ***             ***      ***    ***       ***           ***         ***             \n"+
+        "***    ***    ***        ***        ***               ***           ***             ***    ***      ***       ***           ***         ***             \n"+
+        "*********     ***        ***        ***               ***           **********        ******        ***       ***     **************    ***             \n"
+        
+        
+        );
+    }
 }
