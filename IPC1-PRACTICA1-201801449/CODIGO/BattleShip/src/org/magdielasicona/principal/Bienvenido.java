27c57907dd999affdc6395182106c8fd3de2438f
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-02 23:18:33 -0600

    Se diseño el Menu Principal

--- IPC1-PRACTICA1-201801449/CODIGO/BattleShip/src/org/magdielasicona/principal/Bienvenido.java
+++ IPC1-PRACTICA1-201801449/CODIGO/BattleShip/src/org/magdielasicona/principal/Bienvenido.java
@@ -19,5 +19,8 @@ public class Bienvenido {
         
         
         );
+        
+        MenuPrincipal menu = MenuPrincipal.getInstancia();
+        menu.mostrarMenuPrincipal();
     }
 }
--- IPC1-PRACTICA1-201801449/CODIGO/BattleShip/src/org/magdielasicona/principal/MenuPrincipal.java
+++ IPC1-PRACTICA1-201801449/CODIGO/BattleShip/src/org/magdielasicona/principal/MenuPrincipal.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.magdielasicona.principal;
+
+import java.util.Scanner;
 
 /**
  *
  * @author FRANMAGDIEL_PC
  */
 public class MenuPrincipal {
+    
+     private static MenuPrincipal instanciaMenu;
+     public static MenuPrincipal getInstancia() {
+        if (instanciaMenu == null) {
+            instanciaMenu = new MenuPrincipal();
+        }
+        return instanciaMenu;
+    }
+    
+    
+    Scanner entrada = new Scanner(System.in);
+    public void mostrarMenuPrincipal(){
+      int opcionmenu;
+        do {
+            System.out.println("");
+            System.out.println("        MENU PRINCIPAL");
+            System.out.println("********************************");
+            System.out.println("*    1. CREAR TABLERO          *");
+            System.out.println("*    2. REPORTE COMPLETO       *");
+            System.out.println("*    3. REPORTE VICTORIAS      *");
+            System.out.println("*    4. SALIR                  *");
+            System.out.println("********************************");
+            System.out.println("");
+            opcionmenu = entrada.nextInt();
+            
+            switch(opcionmenu){
+                case 1:
+                    System.out.println("Selecciono 1");
+                    break;
+                case 2: 
+                    System.out.println("Selecciono 2");
+                    break;
+                case 3:
+                    System.out.println("Selecciono 3");
+                    break;
+                case 4:
+                    System.out.println("Selecciono 4");
+                    break;
+                default:
+                    System.out.println("Ingrese una de las Opciones Disponibles!!!!!");
+            }
+        } while (0<opcionmenu && opcionmenu >4);
+        
+    
+    }
     
 }
