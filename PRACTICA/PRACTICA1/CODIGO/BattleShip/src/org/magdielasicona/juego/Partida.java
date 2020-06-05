c2d3d426231c38b34922bbd0a3c40f89c8db0563
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-05 00:40:37 -0600

    Se implemento El metodo de Lanzar Misil

--- PRACTICA/PRACTICA1/CODIGO/BattleShip/src/org/magdielasicona/juego/Partida.java
+++ PRACTICA/PRACTICA1/CODIGO/BattleShip/src/org/magdielasicona/juego/Partida.java
@@ -20,7 +20,9 @@ public class Partida {
     private int dia;
     private int mes;
     private int año;
-    
+    private int intentosDisponibles;
+    private int direccion[] = new int[4];
+
     public static Partida getInstancia() {
         if (instanciaPartida == null) {
             instanciaPartida = new Partida();
@@ -29,7 +31,7 @@ public class Partida {
     }
     Scanner entradaNombres = new Scanner(System.in);
     Calendar calendario = new GregorianCalendar();
-    
+
     public void ingresoNombre() {
 
         System.out.print("INGRESE SU NICKNAME: ");
@@ -64,22 +66,100 @@ public class Partida {
             System.out.println("ERROR, EL TABLERO ESTÁ INCOMPLETO!!!!!");
         }
     }
-    
-    
-    public void empezarJugar(){
-        
+
+    public void empezarJugar() {
+        int opcionDisparar;
+        int contadorIntentos;
         hora = calendario.get(Calendar.HOUR_OF_DAY);
         minuto = calendario.get(Calendar.MINUTE);
         segundo = calendario.get(Calendar.SECOND);
         dia = calendario.get(Calendar.DATE);
         mes = calendario.get(Calendar.MONTH);
         año = calendario.get(Calendar.YEAR);
+
+        //Intentos
+        intentosDisponibles = Controlador.getInstancia().getContadorIntentos();
+        contadorIntentos = Controlador.getInstancia().getContadorIntentos();
         do {
-            System.out.println("HORA: "+dia+"/"+mes+"/"+año+"    "+hora+":"+minuto+":"+segundo);
-            
-        } while (true);
-        
-    
+            System.out.println("HORA: " + dia + "/" + mes + "/" + año + "    " + hora + ":" + minuto + ":" + segundo);
+            System.out.println("");
+            System.out.println("BARCOS");
+            System.out.println("             " + "HUNDIDOS");
+            System.out.println("");
+            System.out.println(contadorIntentos + "/" + intentosDisponibles + " INTENTOS");
+            System.out.println("");
+            System.out.println("-------------------------------------");
+            Controlador.getInstancia().visualizarTablero();
+            System.out.println("");
+            //OPCIONES
+            System.out.println("1. LANZAR MISIL");
+            System.out.println("2. TERMINAR PARTIDA");
+            System.out.print("SELECCIONE UNA OPCION : ");
+            opcionDisparar = entradaNombres.nextInt();
+
+            switch (opcionDisparar) {
+                case 1:
+                    contadorIntentos--;
+                    if (contadorIntentos == 0) {
+                        System.out.println("SE TE ACABARON LOS INTENTOS!!!! ");
+                    } else {
+                        dispararMisil();
+                    }
+
+                    //Metodo disparar
+                    break;
+                case 2:
+                    //Metodo Abandonar
+                    break;
+                default:
+                    System.out.println("SELECCIONE UNA DE LAS OPCIONES DISPONIBLES!!!!!");
+            }
+        } while (0 < opcionDisparar && opcionDisparar > 2);
+
     }
 
+    public void dispararMisil() {
+        String pos[];
+        String coordenada[];
+        String coordenadaAtaque;
+        String cadena="";
+        int valor;
+        
+        int contador;
+        System.out.print("INGRESE LA COORDENADA (FILA,COLUMNA): ");
+        coordenadaAtaque = entradaNombres.next();
+        
+        contador = 0;
+
+        coordenada = coordenadaAtaque.split("-");
+        for (int i = 0; i < coordenada.length; i++) {
+            cadena = quitarParentesis(coordenada[i]);
+            pos = cadena.split(",");
+            for (int j = 0; j < pos.length; j++) {
+                
+                valor = Integer.parseInt(pos[j]);
+                direccion[contador] = valor;
+                contador++;
+            }
+            
+
+        }
+        int a,b;
+            a = direccion[0];
+            b = direccion[1];
+            System.out.println("a _"+a);
+            System.out.println("b _"+b);
+
+    }
+
+    public static String quitarParentesis(String cadena) {
+        String aux = "";
+        for (int i = 1; i < cadena.length() - 1; i++) {
+            aux = aux + cadena.charAt(i);
+            
+
+        }
+        return aux;
+
+    }
 }
