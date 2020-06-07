1d16aab9f3eefee7e38167cfb7da322fbba98c76
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-06 23:46:50 -0600

    Se Implemento las Funcione de Lanzar Misil

--- PRACTICA/PRACTICA1/CODIGO/BattleShip/src/org/magdielasicona/juego/Partida.java
+++ PRACTICA/PRACTICA1/CODIGO/BattleShip/src/org/magdielasicona/juego/Partida.java
@@ -99,15 +99,20 @@ public class Partida {
 
             switch (opcionDisparar) {
                 case 1:
-                    contadorIntentos--;
+                    try {
+
                     if (contadorIntentos == 0) {
                         System.out.println("SE TE ACABARON LOS INTENTOS!!!! ");
                     } else {
+                        contadorIntentos--;
                         dispararMisil();
                     }
+                } catch (Exception e) {
 
-                    //Metodo disparar
-                    break;
+                }
+
+                //Metodo disparar
+                break;
                 case 2:
                     //Metodo Abandonar
                     break;
@@ -131,7 +136,6 @@ public class Partida {
         System.out.print("INGRESE LA COORDENADA (FILA,COLUMNA): ");
         coordenadaAtaque = entradaNombres.next();
 
-        
         contador = 0;
 
         coordenada = coordenadaAtaque.split("-");
@@ -149,93 +153,142 @@ public class Partida {
         int a, b;
         a = direccion[0];
         b = direccion[1];
-        System.out.println("Coor emsablada:"+a +"+"+b);
-        //------------------------------------------------------
-        String valor1="";
-        String valor2="";
-        for (int i = 0; i < 50; i++) {
-            if (coordenadaAtaque.equals(Tablero.coordenadaGuardada[i])) {
-                valor1 = Tablero.coordenadaGuardada[i];
-                valor2 = Tablero.coordenadaGuardada[i+1];
-                System.out.println(valor1);
-                
-                
-                if (valor2==null) {
-                    valor2 = valor1;
-                    System.out.println(valor2);
+
+        String valor1 = "";//(0,0)
+        String valor2 = "";//(1,1)
+
+        if (Tablero.tableroPrincipal[a][b].equals("O")) {
+            //Buscar la coordenada que fue ingresada por obtener la coordenada de ataque
+            for (int i = 0; i < 50; i++) {
+                if (coordenadaAtaque.equals(Tablero.coordenadaGuardada[i])) {
+                    valor1 = Tablero.coordenadaGuardada[i];
+                    valor2 = Tablero.coordenadaGuardada[i + 1];
+
+                    if (valor2 == null) {
+                        valor2 = valor1;
+
+                    }
+
                 }
-              
             }
+        } else if (Tablero.tableroPrincipal[a][b].equals("$")) {
+            for (int i = 0; i < 50; i++) {
+                if (coordenadaAtaque.equals(Tablero.coordenadaGuardada[i])) {
+                    valor1 = Tablero.coordenadaGuardada[i];
+                    valor2 = Tablero.coordenadaGuardada[i + 1];
+
+                    if (valor2 == null) {
+                        valor2 = valor1;
+
+                    }
+
+                }
+            }
+        } else if (Tablero.tableroPrincipal[a][b].equals("-")) {
+            System.out.println("NO LE DÍO A NINGUN BARCO");
+            empezarJugar();
         }
+
+        //------------------------------------------------------
+        //busca la coordenada en el tablero
         //------------------------------------------------------------
         if (valor1 == valor2) {
             contador = 0;
 
-        coordenada = valor1.split("-");
-        for (int i = 0; i < coordenada.length; i++) {
-            cadena = quitarParentesis(coordenada[i]);
-            pos = cadena.split(",");
-            for (int j = 0; j < pos.length; j++) {
+            coordenada = valor1.split("-");
+            for (int i = 0; i < coordenada.length; i++) {
+                cadena = quitarParentesis(coordenada[i]);
+                pos = cadena.split(",");
+                for (int j = 0; j < pos.length; j++) {
 
-                valor = Integer.parseInt(pos[j]);
-                direccion[contador] = valor;
-                contador++;
+                    valor = Integer.parseInt(pos[j]);
+                    direccion[contador] = valor;
+                    contador++;
+                }
+
+            }
+            int x;
+            int y;
+            x = direccion[0];
+            y = direccion[1];
+
+            System.out.println(Tablero.tableroPrincipal[x][y]);
+
+            if (Tablero.tableroPrincipal[x][y].equals("O")) {
+                Tablero.tableroPrincipal[x][y] = "X";
+                System.out.println("LE DIÓ A UN BARCO");
+                empezarJugar();
+
+            } else if (Tablero.tableroPrincipal[x][y].equals("$")) {
+                System.out.println("LE DIÓ AL EASTER EGG");
+                Tablero.tableroPrincipal[x][y] = "X";
+                System.out.println("201801449 Francisco Magdiel Asicona ");
+                empezarJugar();
+
+            }
+
+            //-----------------------------------------------------------------
+            //Coordenadas diferentes (0,0)-(0,3)
+        } else if (valor1 != valor2) {
+            
+            contador = 0;
+
+            coordenada = valor1.split("-");
+            for (int i = 0; i < coordenada.length; i++) {
+                cadena = quitarParentesis(coordenada[i]);
+                pos = cadena.split(",");
+                for (int j = 0; j < pos.length; j++) {
+
+                    valor = Integer.parseInt(pos[j]);
+                    direccion[contador] = valor;
+                    contador++;
+                }
+
+            }
+            int aL;
+            int bL;
+            aL = direccion[0];
+            bL = direccion[1];
+
+            //------------------------------------------------------------------
+            contador = 0;
+
+            coordenada = valor2.split("-");
+            for (int i = 0; i < coordenada.length; i++) {
+                cadena = quitarParentesis(coordenada[i]);
+                pos = cadena.split(",");
+                for (int j = 0; j < pos.length; j++) {
+
+                    valor = Integer.parseInt(pos[j]);
+                    direccion[contador] = valor;
+                    contador++;
+                }
+
+            }
+            int cL;
+            int dL;
+            cL = direccion[0];
+            dL = direccion[1];
+
+            if (Tablero.tableroPrincipal[aL][bL] == "O") {
+                //(0,2)-(0,4)
+                if (aL == cL) {
+                    for (int i = bL; i <= dL; i++) {
+                        Tablero.tableroPrincipal[aL][i] = "X";
+                        System.out.println("LE DIÓ A UN BARCO");
+                    }
+                    empezarJugar();
+                } else if (bL == dL) {
+                    for (int j = aL; j <= cL; j++) {
+                        Tablero.tableroPrincipal[j][bL] = "X";
+                        System.out.println("LE DIÓ A UN BARCO");
+                    }
+                    empezarJugar();
+                }
+
             }
 
         }
-        int x;
-        int y;
-        x = direccion[0];
-        y = direccion[1];
-            System.out.println(x);
-            System.out.println(y);
-        //-----------------------------------------------------------------
-        }else if(valor1!=valor2){
-        contador = 0;
-
-        coordenada = valor1.split("-");
-        for (int i = 0; i < coordenada.length; i++) {
-            cadena = quitarParentesis(coordenada[i]);
-            pos = cadena.split(",");
-            for (int j = 0; j < pos.length; j++) {
-
-                valor = Integer.parseInt(pos[j]);
-                direccion[contador] = valor;
-                contador++;
-            }
-
-        }
-        int x1;
-        int y1;
-        x1 = direccion[0];
-        y1 = direccion[1];
-            System.out.println(x1);
-            System.out.println(y1);
-        //------------------------------------------------------------------
-        contador = 0;
-
-        coordenada = valor2.split("-");
-        for (int i = 0; i < coordenada.length; i++) {
-            cadena = quitarParentesis(coordenada[i]);
-            pos = cadena.split(",");
-            for (int j = 0; j < pos.length; j++) {
-
-                valor = Integer.parseInt(pos[j]);
-                direccion[contador] = valor;
-                contador++;
-            }
-
-        }
-        int x2;
-        int y2;
-        x2 = direccion[0];
-        y2 = direccion[1];
-            System.out.println(x2);
-            System.out.println(y2);
-        }
-        
-        
-        
 
     }
 
