06d3926cef61384fb32ec01dd707db18195c2a66
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-19 16:42:39 -0600

    Se creó el Jframe Menu y la Clase Main

--- PRACTICA/PRACTICA2/CODIGO/Galaga/src/org/magdielasicona/principal/Main.java
+++ PRACTICA/PRACTICA2/CODIGO/Galaga/src/org/magdielasicona/principal/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.magdielasicona.principal;
 
 /**
@@ -10,5 +6,8 @@ package org.magdielasicona.principal;
  * @author FRANMAGDIEL_PC
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
 }
