b739c6f1f4233c575e8fea88dec02a02d857a91a
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-26 17:03:57 -0600

    Se Creo PROYECTO2

---
+++ PROYECTO/PROYECTO2/CODIGO/RecogerBloques/src/org/magdielasicona/menu/Main.java
@@ -0,0 +1,12 @@
+
+package org.magdielasicona.menu;
+
+/**
+ *
+ * @author FRANMAGDIEL_PC
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+    }
+}
