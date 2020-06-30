9fc060b36db09ff615db6aecab72578fc2c0e99d
magdielasicona <3275257541405@ingenieria.usac.edu.gt>
2020-06-29 19:28:53 -0600

    HT4

---
+++ HT/HT4/CODIGO/HT4&Tarea4/src/org/magdielasicona/principal/Iniciador.java
@@ -0,0 +1,17 @@
+
+package org.magdielasicona.principal;
+
+
+/**
+ *
+ * @author FRANMAGDIEL_PC
+ */
+public class Iniciador {
+    public static void main(String[] args) {
+       MenuPrincipal menu = new MenuPrincipal();
+       menu.setVisible(true);
+       
+        
+                
+    }
+}
